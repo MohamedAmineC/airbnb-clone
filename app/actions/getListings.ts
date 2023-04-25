1dc60899bb74e9ead244f4b682f1ff70cc3bdf9d
Mohmaed Amine Chalouati <mohamedaminechalouati@gmail.com>
2023-04-25 20:30:15 +0200

    adding search modal and all the filters to show the listings

--- app/actions/getListings.ts
+++ app/actions/getListings.ts
@@ -2,14 +2,60 @@ import prisma from "@/app/lib/prismadb"
 
 export interface IlistingsParams{
     userId?:string,
+    guestCount?:number,
+    roomCount?:number,
+    bathroomCount?:number,
+    startDate?:string,
+    endDate?:string,
+    locationValue?: string,
+    category?:string
 }
 
 export default async function getListings(params:IlistingsParams){
     try{
-        const {userId} = params
+        const {userId,guestCount,roomCount,bathroomCount,startDate,endDate,locationValue,category} = params
         let query:any = {};
         if(userId){
             query.userId = userId
+        }
+        if(category){
+            query.categorie = category
+        }
+        if(guestCount){
+            query.guestCount = {
+                gte: +guestCount
+            }
+        }
+        if(roomCount){
+            query.roomCount = {
+                gte: +roomCount
+            }
+        }
+        if(bathroomCount){
+            query.bathroomCount = {
+                gte: +bathroomCount
+            }
+        }
+        if(startDate && endDate){
+            query.NOT = {
+                reservations:{
+                    some:{
+                        OR:[
+                            {
+                                endDate: {gte: startDate},
+                                startDate: {lte:startDate}
+                            },
+                            {
+                                startDate:{lte: endDate},
+                                endDate:{gte: endDate}
+                            }
+                        ]
+                    }
+                }
+            }
+        }
+        if(locationValue){
+            query.locationValue = locationValue
         }
         const listings = await prisma.listing.findMany({
             where: query,
